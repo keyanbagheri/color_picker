494b0477061578f8c626bd5be2519f0806ee8cc8
keyanbagherin <keyanbagheri@gmail.com>
2016-04-17 22:21:00 -0400

    updating code with comments

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
 
-
+// creating our color wheel in javascript so we can run functions to append it to DOM
 var colorWheel = [
 	[
 		{rgba: 'rgb(0, 51, 102)', subsets: ['blue', 'dark']},
@@ -163,33 +163,45 @@ $(document).ready(function(){
 	for(var i=0; i<colorWheel.length; i++){
 		$('#color-wheel').append('<div class="color-row" id="row' + i + '"></div>');
 		var row = $('#row' + i)
+		// adding custom width to each row based on number of color circles
 		row.css({
 			width: colorWheel[i].length * 20 + colorWheel[i].length,
 		})
+		// adding the color circles
 		for(var j=0; j<colorWheel[i].length; j++){
+			// giving the circle a unique class based on index values
 			row.append('<div class="color" id="color-' + i + '-' + j + '"></div>');
 			var color = $('#color-' + i + '-' + j)
+			// adding color to the circle
 			color.css({
 				backgroundColor: colorWheel[i][j].rgba
 			});
+			// adding this color's subsets as class names for later reference
 			for(var k=0; k<colorWheel[i][j].subsets.length; k++){
 				color.addClass(colorWheel[i][j].subsets[k]);
 			}
 		}
 	}
 
+	// main function to grab all colors of a subset and display them from outside the circumference towards the inside of the circle
 	function showSubsetColors(subset){
+		// update the text in our input box
 		$('#input').attr("placeholder", 'enter subset (i.e. pastel)');
+		// add a new container for each subset, colors will be added in here
 		$('#colors-display').append('<div class="subset-display" id="' + subset + '"><br><div class="btn btn-default btn-block">' + subset + ' subset</div><div><br>')
-		var colors = []
+		// cycle through the color wheel (starting with the first and last rows)
 		for(var i=0; i<7; i++){
 			$.each(colorWheel, function(index, row) {
+				// if first or last row in current sequence
 				if (index == i || index == (colorWheel.length-1-i)) {
+					// cycle through each color in row
 					for(var j=i; j<row.length-i; j++) {
+						// if color's subsets include current subset append a new div into our subsets container 
 						if ($.inArray(subset, row[j].subsets) >= 0) {
 							var x = $('#' + subset).append('<div class="color-display" style="background: ' + row[j].rgba + '; width: ' + randomDivWidth() + 'px;"></div>');
 						}
 					}
+				// now go through the other rows and check the first and last color of each row (outside - in)
 				} else if (i < index && index < 6) {
 					if ($.inArray(subset, row[i].subsets) >= 0) {
 						var x = $('#' + subset).append('<div class="color-display" style="background: ' + row[i].rgba + '; width: ' + randomDivWidth() + 'px;"></div>')
@@ -210,6 +222,7 @@ $(document).ready(function(){
 		}
 	}
 
+	// event handler for choosing a color directly from the wheel
 	$('#color-wheel').on('click', function(e){
 		// select the div that was clicked on
 		var color = e.target;
@@ -229,30 +242,40 @@ $(document).ready(function(){
 		}
 	})
 
+	// event handler for choosing a color directly from the subset group (or the subset button itself)
 	$('#colors-display').on('click', function(e){
 		var evt = e.target;
+		// check to see if the div clicked was the subset title button
 		if (evt.getAttribute('class') == "btn btn-default btn-block") {
 			$('#input').val($(evt).text().replace(' subset', ''))
 			$('#button').trigger('click');
 		} else {
+			// grabbing background color value from target
 			var rgb = $(evt).css('backgroundColor');
+			// updating display color with above color
 			displayColor(rgb);
 		}
 	})
 
+	// event handler for submitting user subset search
 	$('#button').on('click', function(e){
+		// clearing the subsets containters
 		$('#colors-display').html('');
 		e.preventDefault();
+		// checking for valid input
 		var validSubsets = ['red', 'blue', 'green', 'white', 'light', 'dark', 'pastel', 'brown', 'pink', 'purple', 'orange']
 		$.each(validSubsets, function(index, subset){
+			// if valid update the screen
 			if ($('#input').val() == subset) {
 				showSubsetColors($('#input').val());
 				$('#input').attr("placeholder", subset);
 			}
 		})
+		// clear our input 
 		$('#input').val('');
 	})
 
+	// updates the color displaying in box with logo
 	function displayColor(rgb){
 		$('#color-selected').css({
 			display: 'block',
@@ -261,6 +284,7 @@ $(document).ready(function(){
 		$('#color-selected p').text(rgb);
 	}
 
+	// simple random width [ my algorith ;-) ]
 	function randomDivWidth(){
 		return Math.floor(Math.random()*(101)+20);
 	}
